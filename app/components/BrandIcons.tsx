2f544443f1227f99a6cda5a45d10aa73d9d0042c
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-07-23 12:32:57 -0500

    fix (path): update logos path

--- app/components/BrandIcons.tsx
+++ app/components/BrandIcons.tsx
@@ -4,17 +4,17 @@ type IconProps = { className?: string };
 
 export function UpworkIcon({ className }: IconProps) {
   // eslint-disable-next-line @next/next/no-img-element -- static brand SVG, next/image doesn't optimize SVGs anyway
-  return <img src="/upwork.svg" alt="" aria-hidden="true" className={className} />;
+  return <img src="/logos/upwork.svg" alt="" aria-hidden="true" className={className} />;
 }
 
 export function ContraIcon({ className }: IconProps) {
   // eslint-disable-next-line @next/next/no-img-element -- static brand SVG, next/image doesn't optimize SVGs anyway
-  return <img src="/contra.svg" alt="" aria-hidden="true" className={className} />;
+  return <img src="/logos/contra.svg" alt="" aria-hidden="true" className={className} />;
 }
 
 export function LinkedInIcon({ className }: IconProps) {
   // eslint-disable-next-line @next/next/no-img-element -- static brand SVG, next/image doesn't optimize SVGs anyway
-  return <img src="/linked.svg" alt="" aria-hidden="true" className={className} />;
+  return <img src="/logos/linked.svg" alt="" aria-hidden="true" className={className} />;
 }
 
 export const socialIcons: Record<string, (props: IconProps) => ReactElement> = {
